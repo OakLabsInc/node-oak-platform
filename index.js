7b1d6ac5a4b604497e4efb02e6818b708fc73b30
Flynn Joffray <nucleardreamer@gmail.com>
2018-06-26 19:25:42 -0500

    WIP

--- index.js
+++ index.js
@@ -6,34 +6,82 @@ const grpc = require('grpc')
 const _ = require('lodash')
 const readdirAsync = promisify(readdir)
 
-const PROTOS_PATH = join(__dirname, 'platform-protos')
+const PROTOS_BASE = join(__dirname, 'platform-protos')
 
 class OakPlatform {
-  constructor (root, priv, chain, host = '') {}
-  async listProtoFiles () {
-    try {
-      return _.filter(
-        await readdirAsync(PROTOS_PATH),
-        (file) => file.indexOf('.proto') !== -1
-      )
-    } catch (e) {
-      throw new Error('platform-protos folder does not exist')
-    }
-  }
+  constructor ({ host = 'localhost:443', root, priv, chain }) {
 
-  async protos () {
-    return  _.reduce(
-      await this.listProtoFiles(),
-      (all, file) => {
-        try {
-          return _.merge(
-            all,
-            grpc.load(join(PROTOS_PATH, file))
-          )
-        } catch (e) {
-          return all
+    // create insecure credentials if we have no credential files
+    const credentials = (root && priv && chain) ? grpc.credentials.createSsl(root, priv, chain) : grpc.credentials.createInsecure()
+
+    // wrapping up async instance, you cannot have a class constructor instatiated as an async function
+    return (async () => {
+      let protoFiles = await getProtoFiles()
+      // reduce those proto files to keyed object
+      let res = await Promise.all(protoFiles.map(async (protoPath) => {
+        let result = {}
+        // get the constructed proto
+        let protoObj = await getProtoObject(protoPath)
+        // use the proto file name as the key
+        let protoName = protoPath.replace('.proto', '')
+        // let it sit undefined if there is an error
+        if (_.isError(protoObj)) {
+          result[protoName] = undefined
+          return result
         }
-    }, {})
+
+        result[protoName] = async function () {
+          let final = await connect(host, credentials, protoObj)
+          return final
+        }
+
+        return result
+      }))
+      // merge all into one object
+      return _.assign({}, ...res)
+    })()
+  }
+}
+
+async function connect (host, creds, protoObj) {
+  // extract the actual service in the object
+  let Service = _.find(protoObj, 'service')
+  try {
+    // connect with our previous credentials to this service.
+    // NOTE: this assumes there is one service definition per proto object
+    const service = await new Service(host, creds)
+
+    // extract just the static proto constructors
+    const protos = _.omitBy(protoObj, v => _.has(v, 'service'))
+    _.assign(service, protos)
+    return service
+  } catch (err) {
+    return err
+  }
+}
+
+async function getProtoFiles () {
+  try {
+    let protoFiles = await readdirAsync(PROTOS_BASE)
+    return _.filter(protoFiles, file => file.indexOf('.proto') !== -1)
+  } catch (e) {
+    throw new Error('The protobuf resource directory does not exist')
+  }
+}
+
+async function getProtoObject (PROTO_PATH) {
+  if (!PROTO_PATH) throw new Error('No proto definition specified')
+  try {
+    // load the proto file, and extract the object definitions on oak.platform
+    return _.get(
+      await grpc.load({
+        root: PROTOS_BASE,
+        file: PROTO_PATH
+      }),
+      'oak.platform'
+    )
+  } catch (err) {
+    return new Error(err)
   }
 }
 
--- test/app.test.js
+++ test/app.test.js
@@ -2,15 +2,10 @@ process.env.NODE_ENV = 'test'
 
 const test = require('tape')
 const { join } = require('path')
-let plat = false
+const OakPlatform = require(join(__dirname, '..'))
 
-test('test oak-platform module', function (t) {
-  t.doesNotThrow(function () {
-    plat = require(join(__dirname, '..'))
-  }, 'oak-platform should be requirable')
-
-  (async function () {
-    let platform = new OakPlatform()
-    console.log(await platform.protos())
-  })()
-})
+// test('test oak-platform module', function (t) {
+//   t.doesNotThrow(function () {
+//     plat = require(join(__dirname, '..'))
+//   }, 'oak-platform should be requirable')
+// })
