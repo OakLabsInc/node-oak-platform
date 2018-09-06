498614a60d7413de8cec01d3583cf0fa5516a249
Flynn Joffray <nucleardreamer@gmail.com>
2018-09-06 16:37:37 -0700

    protos update, small tweaks to list example

--- examples/list_platform.js
+++ examples/list_platform.js
@@ -1,5 +1,6 @@
 const { join } = require('path')
 const OakPlatform = require(join(__dirname, '..'))
+const _ = require('lodash')
 
 async function go () {
   let platform = await new OakPlatform({
@@ -8,7 +9,15 @@ async function go () {
   console.log('Platform methods:')
   Object.keys(platform).forEach(v => {
     console.log(`* ${v}`)
-    Object.keys(platform[v].proto).forEach(v => console.log(`  * ${v}`))
+    Object.keys(platform[v].proto).forEach(v => {
+      console.log(`  * ${v}`)
+    })
+    let upper = _.startCase(v).replace(/\s/g, '')
+    if (platform[v].proto[upper]) {
+      Object.keys(platform[v].proto[upper].service).forEach(v => {
+        console.log(`    * ${v}`)
+      })
+    }
   })
 }
 
