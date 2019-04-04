3ea7e18362e4f68cb4bb55b5baeddb658505f4be
Flynn Joffray <nucleardreamer@gmail.com>
2019-04-04 13:59:22 -0700

    proto-loader options. bumping version to 3.0.0

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ class OakPlatform {
   constructor (opts = {}) {
     return (async () => {
       opts = _.defaults(opts, {
+        protoLoaderOptions: {
+          keepCase: true,
+          longs: String,
+          enums: String,
+          defaults: true,
+          oneofs: true
+        },
         basePath: join(__dirname, 'platform-protos')
       })  
       let instance = await new QuickgRPC(opts)
